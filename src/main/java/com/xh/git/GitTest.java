7e8be453d1911e9be87a6d8bfe9cc18bfb40c584
Peng <1595410276@qq.com>
2023-02-21 14:51:35 +0800

    main commit 2

--- src/main/java/com/xh/git/GitTest.java
+++ src/main/java/com/xh/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!!");
         System.out.println("hello hot-fix 1");
+        System.out.println("main text");
     }
 }
